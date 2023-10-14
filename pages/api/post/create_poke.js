91252594b037dff4944d8150ebbae4f7415ebf0b
EdgarGoPr <egdeprada@hotmail.com>
2023-10-14 02:37:49 -0300

    missing data en create pokemon

--- pages/api/post/create_poke.js
+++ pages/api/post/create_poke.js
@@ -10,12 +10,13 @@ export default async function handler(req, res) {
           throw new Error(`Type '${t}' does not exist`);
         }
       });
+      if (!data.ident || !data.name || !data.image || !data.health || !data.attack || !data.defense || !data.speed || !data.height || !data.weight || !data.type) throw new Error('Missing data')
       await Promise.all(typePromises);
       const poke = await prisma.pokemon.create({ data });
       res.status(200).json({ message: 'Pokemon created successfully', data: poke });
     } catch (error) {
       console.log('Error:', error);
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ message: 'Error creating pokemon' });
     }
   } else {
     res.status(405).json({ message: 'Method not allowed' });
