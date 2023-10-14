38ddf1800222f4df6419ce8b34feb3f7c2732ad4
EdgarGoPr <egdeprada@hotmail.com>
2023-10-14 02:00:31 -0300

    update pokemon

--- pages/api/create_poke.js
+++ pages/api/create_poke.js
@@ -1,14 +1,21 @@
 import prisma from "./utils/prisma";
 
 export default async function handler(req, res) {
-  const { data } = req.body
+  const { data } = req.body;
   if (req.method === 'POST') {
     try {
-      const poke = await prisma.pokemon.create({ data: data })
-      res.status(200).json({ message: 'Pokemon created successfully', data: poke })
+      const typePromises = data.type.map(async (t) => {
+        const type = await prisma.type.findUnique({ where: { name: t } });
+        if (!type) {
+          throw new Error(`Type '${t}' does not exist`);
+        }
+      });
+      await Promise.all(typePromises);
+      const poke = await prisma.pokemon.create({ data });
+      res.status(200).json({ message: 'Pokemon created successfully', data: poke });
     } catch (error) {
       console.log('Error:', error);
-      res.status(500).json({ message: 'An error occurred while creating pokemon' });
+      res.status(500).json({ message: error.message });
     }
   } else {
     res.status(405).json({ message: 'Method not allowed' });
