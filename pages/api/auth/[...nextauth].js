b1dfc080465f4a810755f71985d0d6dddc291348
EdgarGoPr <egdeprada@hotmail.com>
2024-07-15 22:14:28 -0300

    enviroments-changed

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,12 +5,12 @@ import GoogleProvider from 'next-auth/providers/google'
 const authOptions = {
     providers: [
         GithubProvider({
-            clientId: 'Iv1.9626e7de1a27bd0d',
-            clientSecret: '87e4997aea2069d7b54643b38ac992179b34af27'
+            clientId: env('GitClient'),
+            clientSecret: env('GitSecret')
         }),
         GoogleProvider({
-            clientId: '376989954887-ge3ribhdl2tb5bl09u3nhmupg0560rr2.apps.googleusercontent.com',
-            clientSecret: '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'
+            clientId: env('GoogleClient'),
+            clientSecret: env('GoogleSecret')
         })
     ],
     callbacks: {
